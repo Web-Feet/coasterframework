1f4b9cab79b6404aa82c7c522fac9b4d73e0c46e
Neil Jennings <neiljennings@ntlworld.com>
2016-03-09 13:13:57 +0000

    rename individual page permissions to make more sense

--- resources/views/admin/partials/roles/pages/li.blade.php
+++ resources/views/admin/partials/roles/pages/li.blade.php
@@ -2,7 +2,7 @@
     <div>
         <span style="min-width:300px;display: inline-block;">{!! $page_lang->name !!}:</span>
         @foreach($actions as $action => $value)
-            {!! Form::checkbox('page['.$page_lang->page_id.']['.$action.']',1,$value,['style' => 'margin-bottom:5px;margin-left:40px;', 'class' => 'page-'.$action]).' ' . str_replace(['index', 'version-publish'], ['list', 'publish'], $action) .' ' !!}
+            {!! Form::checkbox('page['.$page_lang->page_id.']['.$action.']',1,$value,['style' => 'margin-bottom:5px;margin-left:40px;', 'class' => 'page-'.$action]).' ' . str_replace(['index', 'version-publish', 'sort', 'add'], ['list', 'publish', 'sort subpages', 'add subpages'], $action) .' ' !!}
         @endforeach
     </div>
     {!! $sub_pages !!}
