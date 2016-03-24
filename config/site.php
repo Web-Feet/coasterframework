97c367c1608ac62a012f4b35671fe28e5b5ce31d
Neil Jennings <neiljennings@ntlworld.com>
2016-03-24 10:31:50 +0000

    v5.2.25

--- config/site.php
+++ config/site.php
@@ -4,7 +4,7 @@ return [
 
     'name' => 'Coaster CMS',
     'email' => 'info@example.com',
-    'version' => 'v5.2.24',
+    'version' => 'v5.2.25',
     'pages' => '0',
     'secure_folders' => 'secure'
 
