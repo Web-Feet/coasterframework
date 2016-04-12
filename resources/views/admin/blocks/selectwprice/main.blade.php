49d00fe56528a15d8f9cd15028bcfd6bbad5c9c8
Neil Jennings <neiljennings@ntlworld.com>
2016-04-12 16:25:29 +0100

    convert all select fields to use select2

--- resources/views/admin/blocks/selectwprice/main.blade.php
+++ resources/views/admin/blocks/selectwprice/main.blade.php
@@ -3,7 +3,7 @@
 <div class="form-group {!! FormMessage::get_class($name) !!}">
     {!! Form::label($name, $label, ['class' => 'control-label col-sm-2']) !!}
     <div class="col-sm-6">
-        {!! Form::select($name, $content->options, $content->selected, ['class' => 'form-control '.$class]) !!}
+        {!! Form::select($name, $content->options, $content->selected, ['class' => 'form-control chosen-select '.$class]) !!}
         <span class="help-block">{!! FormMessage::get_message($name) !!}</span>
     </div>
     <div class="col-sm-4">
