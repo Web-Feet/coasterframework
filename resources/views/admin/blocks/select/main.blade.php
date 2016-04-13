b622f32f95475c6dbaaaec28323225bc842886d6
Neil Jennings <neiljennings@ntlworld.com>
2016-04-13 13:06:00 +0100

    select width fix

--- resources/views/admin/blocks/select/main.blade.php
+++ resources/views/admin/blocks/select/main.blade.php
@@ -3,7 +3,7 @@
 <div class="form-group {{ $field_class }}">
     {!! Form::label($name, $label, ['class' => 'control-label col-sm-2']) !!}
     <div class="col-sm-10">
-        {!! Form::select($name, $content->options, $submitted_data?:$content->selected, ['class' => 'form-control chosen-select '.$class] + $disabled) !!}
+        {!! Form::select($name, $content->options, $submitted_data?:$content->selected, ['style' => 'width: 100%', 'class' => 'form-control chosen-select '.$class] + $disabled) !!}
         <span class="help-block">{{ $field_message }}</span>
     </div>
 </div>
