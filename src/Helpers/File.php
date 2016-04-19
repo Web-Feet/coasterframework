8552bc518eae08b5456178e6bf39bad1d3fc687b
Neil Jennings <neiljennings@ntlworld.com>
2016-04-19 13:36:51 +0100

    extra file functions

--- src/Helpers/File.php
+++ src/Helpers/File.php
@@ -17,7 +17,8 @@ class File
         }
     }
 
-    public static function copyDirectory($srcDir, $dstDir, $callback = '') {
+    public static function copyDirectory($srcDir, $dstDir, $callback = '')
+    {
         $srcDir = rtrim($srcDir, '/');
         $dstDir = rtrim($dstDir, '/');
         $dir = opendir($srcDir);
@@ -40,6 +41,40 @@ class File
             }
         }
         closedir($dir);
+    }
+
+    public static function insertAtLine($src, array $insertions)
+    {
+        $fileHandle = fopen($src, 'r') or die("couldn't open $src");
+        $lineNo = 1;
+        $newLines = [];
+        $insertLines = array_keys($insertions);
+        while (($currentLine = fgets($fileHandle, 4096)) !== false) {
+
+            if (in_array($lineNo, $insertLines)) {
+                foreach ($insertions[$lineNo] as $line) {
+                    $newLines[] = $line . "\r\n";
+                }
+                $newLines[] = $currentLine;
+            } else {
+                $newLines[] = $currentLine;
+            }
+            $lineNo++;
+        }
+        fclose($fileHandle);
+
+        $fileHandle = fopen($src, 'w') or die("couldn't open $src");
+        foreach ($newLines as $newLine) {
+            fwrite($fileHandle, $newLine);
+        }
+        fclose($fileHandle);
+    }
+
+    public static function replaceString($src, $match, $replace)
+    {
+        $fileContent = file_get_contents($src);
+        $str = str_replace($match, $replace, $fileContent);
+        file_put_contents($src, $str);
     }
 
 }
