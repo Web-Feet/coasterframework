519f0a3b161b107a53f1589a4b19a241d61029b4
Neil Jennings <neiljennings@ntlworld.com>
2016-07-14 12:22:50 +0100

    remove admin error

--- src/Http/routes/admin.php
+++ src/Http/routes/admin.php
@@ -4,8 +4,6 @@ $routeNamePrefix = 'coaster.admin.';
 $admin = config('coaster::admin.url') . '/';
 $adminController = 'CoasterCms\Http\Controllers\AdminControllers\\';
 
-Route::any('{other}', function() {exit;})->where('other', '.*');
-
 Route::group(['middleware' => ['web', 'coaster.guest']], function () use($admin, $adminController, $routeNamePrefix) {
 
     Route::any($admin . 'login', ['uses' => $adminController . 'AuthController@login', 'as' => $routeNamePrefix . 'login']);
