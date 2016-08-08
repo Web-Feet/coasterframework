0d7728c747872a2fec4ce224b9773fbe3e333aa7
Neil Jennings <neil-jennings@users.noreply.github.com>
2016-08-08 11:21:34 +0100

    v5.2.44

--- config/site.php
+++ config/site.php
@@ -4,7 +4,7 @@ return [
 
     'name' => 'Coaster CMS',
     'email' => 'info@example.com',
-    'version' => 'v5.2.42',
+    'version' => 'v5.2.44',
     'pages' => '0',
     'secure_folders' => 'secure',
     'storage_path' => 'app/coaster'
