a4198d58bdc6bfa79e39abe6c9e34b1537347faf
Neil Jennings <neil-jennings@users.noreply.github.com>
2017-02-23 16:00:51 +0000

    Update site.php

--- config/site.php
+++ config/site.php
@@ -4,7 +4,7 @@ return [
 
     'name' => 'Coaster CMS',
     'email' => 'info@example.com',
-    'version' => 'v5.3.28',
+    'version' => 'v5.3.29',
     'pages' => '0',
     'groups' => '0',
     'secure_folders' => 'secure',
