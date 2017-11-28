88e0c0bebbc565109d6f7444e5557e33fffe0308
Neil Jennings <neil-jennings@users.noreply.github.com>
2017-11-28 13:43:11 +0000

    Update CoasterAuthUserProvider.php

--- src/Providers/CoasterAuthUserProvider.php
+++ src/Providers/CoasterAuthUserProvider.php
@@ -1,28 +1,29 @@
 <?php namespace CoasterCms\Providers;
 
-use Config;
-use Hash;
+use Illuminate\Auth\EloquentUserProvider;
 use Illuminate\Contracts\Auth\Authenticatable;
-use Illuminate\Contracts\Auth\UserProvider;
 
-class CoasterAuthUserProvider implements UserProvider
+class CoasterAuthUserProvider extends EloquentUserProvider
 {
 
-    protected $model;
-
-    public function __construct()
-    {
-        $this->model = Config::get('auth.providers.users.model');
-    }
-
+    /**
+     * @param mixed $identifier
+     * @return \Illuminate\Database\Eloquent\Model|null|static
+     */
     public function retrieveByID($identifier)
     {
         return $this->createModel()->newQuery()->where('id', '=', $identifier)->where('active', '=', 1)->first();
     }
 
+    /**
+     * @param mixed $identifier
+     * @param string $token
+     * @return \Illuminate\Database\Eloquent\Model|null|static
+     */
     public function retrieveByToken($identifier, $token)
     {
         $model = $this->createModel();
+
         return $model->newQuery()
             ->where($model->getKeyName(), $identifier)
             ->where($model->getRememberTokenName(), $token)
@@ -30,27 +31,23 @@ class CoasterAuthUserProvider implements UserProvider
             ->first();
     }
 
+    /**
+     * @param Authenticatable $user
+     * @param string $token
+     */
     public function updateRememberToken(Authenticatable $user, $token)
     {
         $user->setRememberToken($token);
         $user->save();
     }
 
+    /**
+     * @param array $credentials
+     * @return \Illuminate\Database\Eloquent\Model|null|static
+     */
     public function retrieveByCredentials(array $credentials)
     {
         return $this->createModel()->newQuery()->where('email', '=', $credentials['username'])->where('active', '=', 1)->first();
-    }
-
-    public function validateCredentials(Authenticatable $user, array $credentials)
-    {
-        return Hash::check($credentials['password'], $user->getAuthPassword());
-    }
-
-    public function createModel()
-    {
-        $class = '\\' . ltrim($this->model, '\\');
-
-        return new $class;
     }
 
 }
