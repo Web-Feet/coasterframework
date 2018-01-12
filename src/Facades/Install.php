c82039df1b50bc601aea29be2c457c6171c1b34d
Dan Chadwick <chadanuk@gmail.com>
2018-01-12 16:57:18 +0000

    Add test features to Coaster framework

--- src/Facades/Install.php
+++ src/Facades/Install.php
@@ -11,19 +11,26 @@ use Illuminate\Support\Facades\Facade;
 class Install extends Facade
 {
 
+<<<<<<< HEAD
     /**
      * @return string
      */
+=======
+>>>>>>> Add test features to Coaster framework
     protected static function getFacadeAccessor()
     {
         return InstallContract::class;
     }
 
+<<<<<<< HEAD
     /**
      * @return string
      */
     protected static function getMockableClass()
     {
+=======
+    protected static function getMockableClass() {
+>>>>>>> Add test features to Coaster framework
         return self::getFacadeAccessor();
     }
 }
