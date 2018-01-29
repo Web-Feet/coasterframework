19b4ac7bccb7293a93ed635a237053e1893525f0
Dan Chadwick <chadanuk@gmail.com>
2018-01-29 11:41:11 +0000

    Add string disaply view test

---
+++ tests/Feature/Blocks/StringBlockTest.php
@@ -0,0 +1,103 @@
+<?php
+
+use CoasterCms\Facades\PageBuilder as PageBuilderFacade;
+use CoasterCms\Libraries\Blocks\String_;
+use CoasterCms\Models\Block;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Mockery as m;
+use Tests\Feature\Blocks\Traits\BlockTrait;
+use Tests\TestCase;
+
+
+class StringBlockTest extends TestCase
+{
+    use BlockTrait;
+    /**
+     * @test
+     * @codingStandardsIgnoreLine */
+    public function display_renders_a_string()
+    {
+        Block::unguard();
+        $block = new Block;
+        $block->content = 'A string';
+        $stringClass = new String_($block);
+
+        $this->assertEquals('A string', $stringClass->display($block->content));
+    }
+
+    /**
+     * @test
+     * @codingStandardsIgnoreLine */
+    function can_display_through_a_view()
+    {
+        Block::unguard();
+        $block = new Block;
+        $block->content = 'string';
+        $stringClass = new String_($block);
+
+        // Create block view
+        $this->createView('strings', 'The {{ $data }}');
+
+        $this->assertEquals('The string', $stringClass->display($block->content, ['view' => 'strings.default']));
+    }
+
+    /**
+     * @test
+     * @codingStandardsIgnoreLine */
+    function data_returns_string()
+    {
+        Block::unguard();
+        $block = new Block;
+        $block->content = 'A string';
+        $stringClass = new String_($block);
+
+        $this->assertEquals('A string', $stringClass->data($block->content));
+    }
+
+    /**
+     * @test
+     * @codingStandardsIgnoreLine */
+    function page_name_can_be_passed_into_string_when_meta_option_is_true()
+    {
+        PageBuilderFacade::shouldReceive('getData');
+        PageBuilderFacade::shouldReceive('pageName')->andReturn('The page');
+
+        Block::unguard();
+        $block = new Block;
+        $block->content = 'A string %page_name%';
+        $stringClass = new String_($block);
+
+        $this->assertEquals('A string The page', $stringClass->display($block->content, ['meta' => true]));
+    }
+
+    /**
+     * @test
+     * @codingStandardsIgnoreLine */
+    function site_name_can_be_passed_into_string_when_meta_option_is_true()
+    {
+        config(['coaster::site.name' => 'The site']);
+        PageBuilderFacade::shouldReceive('getData')->shouldReceive('pageName');
+
+        Block::unguard();
+        $block = new Block;
+        $block->content = 'A string %site_name%';
+        $stringClass = new String_($block);
+
+        $this->assertEquals('A string The site', $stringClass->display($block->content, ['meta' => true]));
+    }
+
+    /**
+     * @test
+     * @codingStandardsIgnoreLine */
+    function if_meta_strip_tags()
+    {
+        Block::unguard();
+        $block = new Block;
+        $block->content = 'A string <h3>In a tag</h3>';
+        $stringClass = new String_($block);
+
+        $this->assertEquals('A string In a tag', $stringClass->display($block->content, ['meta' => true]));
+    }
+}
