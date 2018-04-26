11e15d48582998b6b4382d33764d4d89c181f698
Neil Jennings <neil@web-feet.co.uk>
2018-04-26 09:28:41 +0100

    version

--- config/site.php
+++ config/site.php
@@ -4,7 +4,7 @@ return [
 
     'name' => 'Coaster CMS',
     'email' => 'info@example.com',
-    'version' => 'v5.5.4',
+    'version' => 'v5.5.5',
     'pages' => '0',
     'groups' => '0',
     'secure_folders' => 'secure',
