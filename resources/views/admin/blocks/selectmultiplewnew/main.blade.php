0b60620220d10eaa96511c1a48b6310b2ce7f941
Neil Jennings <neil-jennings@users.noreply.github.com>
2019-10-04 13:16:13 +0100

    Update main.blade.php

--- resources/views/admin/blocks/selectmultiplewnew/main.blade.php
+++ resources/views/admin/blocks/selectmultiplewnew/main.blade.php
@@ -2,7 +2,7 @@
     {!! Form::label($name, $label, ['class' => 'control-label col-sm-2']) !!}
     <div class="col-sm-5">
         {!! Form::hidden($name . '[exists]', 1) !!}
-        {!! Form::select($name . '[select][]', $selectOptions, $submitted_data?:$content, ['style' => 'width: 100%', 'class' => 'chosen-select '.$class, 'multiple' => 'multiple']) !!}
+        {!! Form::select($name . '[select][]', $selectOptions, $submitted_data?$submitted_data['select']:$content, ['style' => 'width: 100%', 'class' => 'chosen-select '.$class, 'multiple' => 'multiple']) !!}
     </div>
     <div class="col-sm-5">
         {!! Form::text($name . '[custom]', '', ['class' => 'form-control', 'placeholder' => 'Add new options (comma separated)']) !!}
