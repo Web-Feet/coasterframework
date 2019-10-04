e4acfa992b4dc264db89a467398d14abca8290ec
Neil Jennings <neil-jennings@users.noreply.github.com>
2019-10-04 13:16:40 +0100

    Update main.blade.php

--- resources/views/admin/blocks/selectclass/main.blade.php
+++ resources/views/admin/blocks/selectclass/main.blade.php
@@ -2,7 +2,7 @@
     {!! Form::label($name, $label, ['class' => 'control-label col-sm-2']) !!}
     <div class="col-sm-10">
         {!! Form::hidden($name . '[exists]', 1) !!}
-        {!! Form::select($name . '[select]', $selectOptions, $submitted_data?:$content, ['style' => 'width: 100%', 'class' => 'form-control chosen-select-class '.$class] + $disabled) !!}
+        {!! Form::select($name . '[select]', $selectOptions, $submitted_data?$submitted_data['select']:$content, ['style' => 'width: 100%', 'class' => 'form-control chosen-select-class '.$class] + $disabled) !!}
         <span class="help-block">{{ $field_message }}</span>
     </div>
 </div>
