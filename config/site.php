ae93e872540ff24b541983668e846440412f8d5e
Dan Chadwick <chadanuk@gmail.com>
2020-02-26 11:05:40 +0000

    Bump version

--- config/site.php
+++ config/site.php
@@ -4,7 +4,7 @@ return [
 
     'name' => 'Coaster CMS',
     'email' => 'info@example.com',
-    'version' => 'v5.5.15',
+    'version' => 'v6.0.0',
     'pages' => '0',
     'groups' => '0',
     'secure_folders' => 'secure',
