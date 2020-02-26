609c40c1283f91c97fd47504e37f49780d686937
Dan Chadwick <chadanuk@gmail.com>
2020-02-26 11:04:57 +0000

    Fix string hlper call to random

--- src/Helpers/Cms/StringHelper.php
+++ src/Helpers/Cms/StringHelper.php
@@ -1,4 +1,8 @@
-<?php namespace CoasterCms\Helpers\Cms;
+<?php
+
+namespace CoasterCms\Helpers\Cms;
+
+use Illuminate\Support\Str;
 
 class StringHelper
 {
@@ -8,10 +12,9 @@ class StringHelper
         if (strlen($string) < $length) {
             return $string;
         } else {
-            $str = str_random(15);
+            $str = Str::random(15);
             $string =  substr($string, 0, strpos(wordwrap($string, $length, "/$str/"), "/$str/")) . ' ...';
             return substr($string, -1) == '.' ? $string : $string . ' ...';
         }
     }
-
 }
